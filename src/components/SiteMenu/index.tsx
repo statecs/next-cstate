374fd3c524a86e1529c6251ab81804e2a4927015
statecs <statec@icloud.com>
2024-04-02 15:21:28 +0200

    Border-top color

--- src/components/SiteMenu/index.tsx
+++ src/components/SiteMenu/index.tsx
@@ -5,7 +5,7 @@ const SiteMenu: React.FC = async () => {
     return (
         <nav className="mb-16 space-y-0.5 py-8 sm:space-y-0 md:sticky md:top-16 md:py-0">
             <div>
-                <div className="mt-2 max-w-[160px] border-t-2 border-gray-200 pt-2 dark:border-gray-800">
+                <div className="mt-2 max-w-[160px] border-t border-gray-200 pt-2 dark:border-zinc-700">
                     <SiteMenuLink title="All collections" url="/collections" />
                 </div>
             </div>
