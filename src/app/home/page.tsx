673bbae5ab1347c5059803521b38df8f7104ea67
statecs <statec@icloud.com>
2024-04-03 16:48:38 +0200

    Profile on homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,6 +2,7 @@ import config from '@/utils/config';
 import ComboBox from '@/components/ComboBox';
 import {fetchEditorialPage} from '@/utils/contentful';
 import {getEditorialSeo} from '@/utils/helpers';
+import Image from 'next/image';
 
 const HomePage = async () => {
     const page = await fetchEditorialPage('home') || {};
@@ -9,9 +10,23 @@ const HomePage = async () => {
     return (
      <div className="flex h-[calc(100vh-110px)] flex-grow border-spacing-4 items-center justify-center">
         <div className="flex flex-col space-y-2 text-center">
-            <h1 className="font-serif text-xl text-black sm:text-2xl dark:text-white">Christopher State</h1>
+            <div className="link-card inline-flex justify-center items-center gap-2 p-2">
+            <Image
+                src="/images/me.jpeg"
+                alt="Christopher State"
+                width={120}
+                height={120}
+                loading="lazy"
+                className="rounded-full border shadow-sm"
+                />
+            </div>
+            <div className="link-card inline-flex justify-center items-center gap-2 p-2">
+                <div className="flex flex-col">
+                    <h1 className="font-serif text-xl text-black sm:text-2xl dark:text-white">Christopher State</h1>
+                    <span className="text-gray-400">Design Technologist</span>
+                </div>
+            </div>
             <div className="prose-sm leading-relaxed tracking-wide dark:prose-invert prose-p:text-gray-500 dark:prose-p:text-gray-300 px-5 md:px-0 font-serif ">
-                <p>Design Technologist</p>
                 <ComboBox threadId={process.env.THREAD_ID || ''} assistantId={process.env.ASSISTANT_ID || ''}  />
             </div>
         </div>
