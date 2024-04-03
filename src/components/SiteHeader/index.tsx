6efd8464cbaa757abb2d2e7d5f7b746b582d7d99
statecs <statec@icloud.com>
2024-04-03 23:00:02 +0200

    HomeIcon on pathname !== '/'

--- src/components/SiteHeader/index.tsx
+++ src/components/SiteHeader/index.tsx
@@ -1,16 +1,26 @@
 'use client';
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { MobileDrawer } from '@/components/MobileDrawer'
+import { HomeIcon } from 'lucide-react'
 
 const SiteHeader: React.FC = () => {
+    const pathname = usePathname();
 
     return (
         <header className="sticky inset-x-0 top-0 z-10 mx-auto flex h-12 w-full shrink-0 items-center overflow-hidden border-b dark:border-zinc-700 bg-white dark:bg-custom-light-gray dark:text-white text-sm font-medium lg:hidden">
-        <div className="flex h-full w-full items-center px-3 justify-end">
-            <MobileDrawer />
-        </div>
-    </header>
+            <div className="flex h-full w-full items-center px-1 justify-start">    
+                <Link href="/" passHref>
+                    <div className="link-card inline-flex items-center gap-2 p-2">
+                        {pathname !== '/' && <HomeIcon size={20} />}
+                    </div>
+                </Link>
+            </div>
+            <div className="flex h-full w-full items-center px-3 justify-end">
+                <MobileDrawer />
+            </div>
+        </header>
     );
 };
 
