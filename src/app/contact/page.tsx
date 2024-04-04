18e4f73e100571d50eeee86788b3d2d610180086
statecs <statec@icloud.com>
2024-04-04 14:03:08 +0200

    Footer update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,6 @@
 import PageHeader from '@/components/PageHeader';
+import Image from 'next/image';
+import Link from 'next/link';
 import config from '@/utils/config';
 import {fetchEditorialPage} from '@/utils/contentful';
 import {getEditorialSeo} from '@/utils/helpers';
@@ -8,10 +10,33 @@ const ContactPage = async () => {
 
     return (
         <div className="flex flex-grow h-[calc(100vh-110px)] border-spacing-4 py-4 px-3 md:justify-center">
-            <div className="flex flex-col space-y-2">
-                <PageHeader title={page.pageTitle} description={page.content} />
+        <div className="flex flex-col space-y-2">
+            <div className="grid w-full max-w-[700px] gap-4 lg:grid-cols-4 lg:gap-10">
+                <div className="lg:col-span-3">
+                    <PageHeader description={page.content} title={page.pageTitle} />
+                    <h1 className="text-xl font-serif dark:text-white">
+                        <Link href={page.ctaUrl} title={page.ctaLabel}>
+                            {page.ctaLabel}
+                        </Link>
+                    </h1>
+                </div>
+               
+                <Image
+                    alt=""
+                    className="max-w-full sm:max-w-[260px] lg:mt-20"
+                    height={page.photo?.height}
+                    placeholder="empty"
+                    priority={false}
+                    quality={90}
+                    src={page.photo?.url}
+                    width={page.photo?.width}
+                />
             </div>
         </div>
+    </div>
+
+
+      
     );
 };
 
