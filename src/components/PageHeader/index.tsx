8e5da652e08ba6d3b7072359bea57f9852dbc4de
statecs <statec@icloud.com>
2024-04-06 16:58:46 +0200

    Place ctaButton in the right top corner

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -114,61 +114,60 @@ const PageHeader: React.FC<Props> = ({
             className={clsx('pt-2', { 'pb-4 sm:pb-8': hasBottomPadding, 'animate-fadeIn': animate })}
             id="hero"
         >
-            {title && (
-                <>
-                    {backUrl ? (
-                        <Link
-                            href={'/projects/' + backUrl}
-                            className="group space-x-2 focus:outline-dotted focus:outline-2 focus:outline-offset-2 focus:outline-black sm:inline-flex sm:items-baseline"
-                        >
-                            <h1 className="max-w-5xl space-x-2 text-balance break-normal font-serif text-xl text-black underline-offset-4 group-hover:underline sm:text-2xl md:max-w-5xl md:text-3xl dark:text-white">
-                                <span>{title}</span>
-                            </h1>
-                        </Link>
-                    ) : (
-                        <h1 className="max-w-5xl space-x-2 text-balance break-normal font-serif text-xl text-black underline-offset-4 group-hover:underline sm:text-2xl md:max-w-5xl md:text-3xl dark:text-white">
-                            <span>{title}</span>
-                        </h1>
-                        
+            <div className="flex flex-col sm:flex-row justify-between items-start">
+                <div>
+                    {title && (
+                        <>
+                            {backUrl ? (
+                                <Link
+                                    href={'/projects/' + backUrl}
+                                    className="group space-x-2 focus:outline-dotted focus:outline-2 focus:outline-offset-2 focus:outline-black sm:inline-flex sm:items-baseline"
+                                >
+                                    <h1 className="max-w-5xl space-x-2 text-balance break-normal font-serif text-xl text-black underline-offset-4 group-hover:underline sm:text-2xl md:max-w-5xl md:text-3xl dark:text-white">
+                                        <span>{title}</span>
+                                    </h1>
+                                </Link>
+                            ) : (
+                                <h1 className="max-w-5xl space-x-2 text-balance break-normal font-serif text-xl text-black underline-offset-4 group-hover:underline sm:text-2xl md:max-w-5xl md:text-3xl dark:text-white">
+                                    <span>{title}</span>
+                                </h1>
+                            )}
+                        </>
                     )}
-                </>
-            )}
-            <div className="text-white pt-2 text-sm">
-                <span>{formattedDate}</span>
-            </div>
-            {(children || description || ctaUrl) && (
-                <>
-                { typeof description !== 'string' && description?.json &&
-                    <div className="mt-4 md:mt-6">
+                    <div className="text-white pt-2 text-sm">
+                        <span>{formattedDate}</span>
+                    </div>
+                </div>
+
+                {ctaLabel && ctaUrl && typeof description !== 'string' && description?.json && (
+                    <Button
+                        className="mt-2 sm:mt-0 px-0 py-0 sm:text-sm sm:px-4 sm:py-3" // Adjusted text size and padding
+                        href={getExternalUrl(ctaUrl)}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                    >
+                        {ctaLabel}
+                    </Button>
+                )}
+                </div>
+            
+            {(children || description) && (
+                <div className="mt-4 md:mt-6">
                     {description && typeof description !== 'string' && description.json && (
                         <div className="prose-sm max-w-2xl text-balance leading-relaxed tracking-wide lg:prose-base dark:prose-invert prose-p:text-gray-500 lg:max-w-5xl lg:prose-p:leading-relaxed lg:prose-p:tracking-wide dark:prose-p:text-gray-400">
                             {documentToReactComponents(description.json, contentfulRenderOptions(description.links))}
                         </div>
                     )}
-                    
-                    {ctaLabel && ctaUrl && (
-                        <Button
-                            className="mt-4"
-                            href={getExternalUrl(ctaUrl)}
-                            rel="noopener noreferrer"
-                            target="_blank"
-                        >
-                            {ctaLabel}
-                        </Button>
+
+                    {description && typeof description == 'string' && (
+                        <div className="dark:text-gray-300 prose-sm max-w-2xl text-balance leading-relaxed tracking-wide lg:prose-base dark:prose-invert prose-p:text-gray-500 lg:max-w-5xl lg:prose-p:leading-relaxed lg:prose-p:tracking-wide dark:prose-p:text-gray-400">
+                            {description}
+                        </div>
+                        
                     )}
+                    
                     {children}
                 </div>
-                
-                }
-
-                <div>
-                     {description && typeof description == 'string' && description && (
-                        <div className="dark:text-gray-300 prose-sm max-w-2xl text-balance leading-relaxed tracking-wide lg:prose-base dark:prose-invert prose-p:text-gray-500 lg:max-w-5xl lg:prose-p:leading-relaxed lg:prose-p:tracking-wide dark:prose-p:text-gray-400">
-                           {description}
-                        </div>
-                    )}
-                </div>
-                </>
             )}
         </div>
     );
