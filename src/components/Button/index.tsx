9ea4e2db649e7332408c9ea1cfc8144cab634112
statecs <statec@icloud.com>
2024-04-06 17:06:00 +0200

    Fix padding button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 
 const Button: React.FC<React.PropsWithChildren<Props>> = ({children, href, ...props}) => {
-    const className = `inline-block px-4 py-2.5 text-[10px] uppercase tracking-[0.5px] text-gray-700 underline-offset-4 transition duration-200 ease-out hover:bg-gray-200 hover:underline md:text-xs md:font-semibold md:tracking-[1px] inline-flex items-center space-x-2 justify-center dark:text-white dark:hover:bg-gray-800 ${
+    const className = `inline-block sm:px-4 py-2.5 text-[10px] uppercase tracking-[0.5px] text-gray-700 underline-offset-4 transition duration-200 ease-out hover:bg-gray-200 hover:underline md:text-xs md:font-semibold md:tracking-[1px] inline-flex items-center space-x-2 justify-center dark:text-white dark:hover:bg-gray-800 ${
         props.className || ''
     }`;
 
