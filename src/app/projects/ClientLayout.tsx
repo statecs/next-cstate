3962673fc8b63a5429ab185da93adae1d2e00f71
statecs <statec@icloud.com>
2024-09-28 12:32:12 +0200

    Set focus on the h1 heading when navigating

--- src/app/projects/ClientLayout.tsx
+++ src/app/projects/ClientLayout.tsx
@@ -1,9 +1,9 @@
-// ClientLayout.tsx
 'use client';
 
 import React, { ReactNode, useState, useEffect, useRef } from 'react';
 import { SideMenu } from '@/components/SideMenu/SideMenu';
 import { ListLayout } from '@/components/ListLayout/ListLayout';
+import { usePathname } from 'next/navigation';
 
 interface ClientLayoutProps {
   children: ReactNode;
@@ -13,25 +13,46 @@ interface ClientLayoutProps {
 const ClientLayout: React.FC<ClientLayoutProps> = ({ children, posts }) => {
   const [isListVisible, setIsListVisible] = useState(true);
   const listRef = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
+  const pathname = usePathname();
 
+  useEffect(() => {
+    if (contentRef.current) {
+      const h1Element = contentRef.current.querySelector('h1');
+
+      if (h1Element instanceof HTMLElement) {
+        h1Element.tabIndex = -1; 
+        h1Element.classList.add('outline-none'); 
+        h1Element.focus();
+      } else {
+        const focusableElement = contentRef.current.querySelector('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+        if (focusableElement instanceof HTMLElement) {
+          focusableElement.focus();
+        }
+      }
+    }
+  }, [isListVisible, pathname]);
 
   return (
     <div className="flex">
-    <div 
-      ref={listRef}
-      className={`transition-all duration-300 ${isListVisible ? 'w-128' : 'w-0'} overflow-scroll`}
-      aria-hidden={!isListVisible}
-    >
-      <SideMenu title="Projects" isInner>
-        <ListLayout list={posts} isMobile />
-      </SideMenu>
-    </div>
-    <div className="lg:bg-dots flex-1 h-[calc(100vh-110px)] lg:h-[calc(100vh)] overflow-hidden">
-      <div className="lg:bg-dots flex-1 h-[calc(100vh-110px)] lg:h-[calc(100vh)] overflow-scroll">
-        {children}
+      <div 
+        ref={listRef}
+        className={`transition-all duration-300 ${isListVisible ? 'w-128' : 'w-0'} overflow-scroll`}
+        aria-hidden={!isListVisible}
+      >
+        <SideMenu title="Projects" isInner>
+          <ListLayout list={posts} isMobile />
+        </SideMenu>
+      </div>
+      <div className="lg:bg-dots flex-1 h-[calc(100vh-110px)] lg:h-[calc(100vh)] overflow-hidden">
+        <div 
+          ref={contentRef}
+          className="lg:bg-dots flex-1 h-[calc(100vh-110px)] lg:h-[calc(100vh)] overflow-scroll"
+        >
+          {children}
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
