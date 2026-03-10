7f1e7105fec5857b9d0708e8f5ef373538fc4b31
statecs <statec@icloud.com>
2026-03-10 19:47:00 +0100

    Logos

--- src/components/ClientLogos/index.tsx
+++ src/components/ClientLogos/index.tsx
@@ -10,15 +10,12 @@ interface Company {
 }
 
 const companies: Company[] = [
-  { name: 'Cartina', url: 'https://cartina.se/' },
-  { name: 'Vattenfall', url: 'https://www.vattenfall.se/' },
-  { name: 'SJ', url: 'https://www.sj.se/' },
-  { name: 'ICA', url: 'https://www.icagruppen.se/' },
-  { name: 'Mapiful', url: 'https://www.mapiful.com/' },
-  { name: 'Doctrin', url: 'https://www.doctrin.se/' },
-  { name: 'THS', url: 'https://ths.kth.se/' },
-  { name: 'KTH', url: 'https://www.kth.se/' },
-  { name: 'State Creative' },
+  { name: 'Cartina', logo: '/images/logos/cartina.svg', url: 'https://cartina.se/' },
+  { name: 'Vattenfall', logo: '/images/logos/Vattenfall_logo2.svg', url: 'https://www.vattenfall.se/' },
+  { name: 'SJ', logo: '/images/logos/SJ.svg', url: 'https://www.sj.se/' },
+  { name: 'ICA', logo: '/images/logos/ICA_logo.svg', url: 'https://www.icagruppen.se/' },
+  { name: 'Axfood', logo: '/images/logos/AXFO.ST_BIG.svg', url: 'https://www.axfood.se/' },
+  { name: 'KTH', logo: '/images/logos/KTH_logo_RGB_vit.svg', url: 'https://www.kth.se/' },
 ];
 
 const ClientLogos: React.FC = () => {
@@ -36,7 +33,7 @@ const ClientLogos: React.FC = () => {
         {companies.map((company) => (
           <div
             key={company.name}
-            className="aspect-square rounded-xl border border-gray-800 dark:border-gray-700 flex items-center justify-center bg-gray-900/50 dark:bg-gray-800/50 hover:bg-gray-800 dark:hover:bg-gray-700 hover:border-gray-700 dark:hover:border-gray-600 transition-all duration-300 hover:scale-105 group p-6"
+            className="aspect-square rounded-xl border border-gray-200/30 dark:border-gray-700/30 flex items-center justify-center bg-transparent hover:bg-gray-100/50 dark:hover:bg-gray-800/50 hover:border-gray-300/50 dark:hover:border-gray-600/50 transition-all duration-300 hover:scale-105 group p-6"
           >
             {company.logo ? (
               <div className="relative w-full h-full flex items-center justify-center">
@@ -45,7 +42,7 @@ const ClientLogos: React.FC = () => {
                   alt={`${company.name} logo`}
                   width={120}
                   height={120}
-                  className="object-contain filter brightness-0 invert dark:brightness-100 dark:invert-0 opacity-70 group-hover:opacity-100 transition-opacity duration-300"
+                  className="object-contain filter brightness-0 dark:invert opacity-70 group-hover:opacity-100 transition-opacity duration-300"
                 />
               </div>
             ) : (
